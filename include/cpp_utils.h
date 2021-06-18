c95dce9d7d607d7c194df3a20b1c31ed8483fac8
Charly Mourglia <charly.mourglia@gmail.com>
2021-06-19 01:14:07 +0200

    Added stuff + prefixed macros

--- include/cpp_utils.h
+++ include/cpp_utils.h
@@ -1,88 +1,90 @@
 #pragma once
 
 // Macro helpers
-#define global static
-#define local static
+#define global_variable static
+#define local_variable static
 
-#define COMPILER_MSVC 0
-#define COMPILER_CLANG 0
-#define COMPILER_GCC 0
+#define Z_COMPILER_MSVC 0
+#define Z_COMPILER_CLANG 0
+#define Z_COMPILER_GCC 0
 
 // Clang is first to avoid problems with clang on windows
 #if defined(__clang__)
-#	undef COMPILER_CLANG
-#	define COMPILER_CLANG (__clang_major__ * 10000 + __clang_minor__ * 100 + __clang_patchlevel__)
+#	undef Z_COMPILER_CLANG
+#	define Z_COMPILER_CLANG (__clang_major__ * 10000 + __clang_minor__ * 100 + __clang_patchlevel__)
 #elif defined(_MSC_VER)
-#	undef COMPILER_MSVC
-#	define COMPILER_MSVC _MSC_VER
+#	undef Z_COMPILER_MSVC
+#	define Z_COMPILER_MSVC _MSC_VER
 #elif defined(__GNUC__)
-#	undef COMPILER_GCC
-#	define COMPILER_GCC (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
+#	undef Z_COMPILER_GCC
+#	define Z_COMPILER_GCC (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
 #else
 #	error "Undefined compiler"
 #endif
 
-#define PLATFORM_WINDOWS 0
-#define PLATFORM_LINUX 0
-#define PLATFORM_MACOS 0 // TODO
+#define Z_PLATFORM_WINDOWS 0
+#define Z_PLATFORM_LINUX 0
+#define Z_PLATFORM_MACOS 0 // TODO
 
 #if defined(_WIN32) || defined(_WIN64)
 #	ifndef NOMINMAX
 #		define NOMINMAX
 #	endif
-#	undef PLATFORM_WINDOWS
-#	define PLATFORM_WINDOWS 1
+#	undef Z_PLATFORM_WINDOWS
+#	define Z_PLATFORM_WINDOWS 1
 #elif defined(__linux__)
-#	undef PLATFORM_LINUX
-#	define PLATFORM_LINUX 1
+#	undef Z_PLATFORM_LINUX
+#	define Z_PLATFORM_LINUX 1
 #else
 #	error "Undefined platform"
 #endif
 
 #include <stdint.h>
 #if UINTPTR_MAX == 0xffffffffffffffff
-#	define ARCH64
+#	define Z_ARCH64
 #elif UINTPTR_MAX == 0xffffffff
-#	define ARCH32
+#	define Z_ARCH32
 #else
 #	error "Unsupported architecture"
 #endif
 
-#define DEBUG 0
-#define RELWITHDEBINFO 0
-#define RELEASE 0
+#define Z_DEBUG 0
+#define Z_RELWITHDEBINFO 0
+#define Z_RELEASE 0
 
 #if defined(_DEBUG)
-#	undef DEBUG
-#	define DEBUG 1
+#	undef Z_DEBUG
+#	define Z_DEBUG 1
 #elif defined(_RELWITHDEBINFO)
-#	undef RELWITHDEBINFO
-#	define RELWITHDEBINFO 1
+#	undef Z_RELWITHDEBINFO
+#	define Z_RELWITHDEBINFO 1
 #else
-#	undef RELEASE
-#	define RELEASE 1
+#	undef Z_RELEASE
+#	define Z_RELEASE 1
 #endif
 
-#define UNUSED(x) ((void)x)
+#define Z_UNUSED(x) ((void)x)
 
-#if COMPILER_GCC || COMPILER_CLANG
-#	define ALIGN(x, a) x __attribute__((aligned(a))
-#	define BREAKPOINT __asm__ volatile("int $0x03")
-#	define LIKELY(x) __builtin_expect(!!(x), 1)
-#elif COMPILER_MSVC
-#	define ALIGN(x, a) __declspec(align(a)) x
-#	define BREAKPOINT __debugbreak()
-#	define LIKELY(x) (x)
+#if Z_COMPILER_GCC || Z_COMPILER_CLANG
+#	define Z_ALIGN(x, a) x __attribute__((aligned(a))
+#	define Z_BREAKPOINT __asm__ volatile("int $0x03")
+#	define Z_LIKELY(x) __builtin_expect(!!(x), 1)
+#	define Z_NO_VTABLE
+#elif Z_COMPILER_MSVC
+#	define Z_ALIGN(x, a) __declspec(align(a)) x
+#	define Z_BREAKPOINT __debugbreak()
+#	define Z_LIKELY(x) (x)
+#	define Z_NO_VTABLE __declspec(novtable)
 #endif
 
 // TODO: Add a message to the assert
-#if DEBUG
+#if Z_DEBUG
 #	define Assert(x)                                                                                                  \
 		do                                                                                                             \
 		{                                                                                                              \
-			if (!LIKELY(x))                                                                                            \
+			if (!Z_LIKELY(x))                                                                                          \
 			{                                                                                                          \
-				BREAKPOINT;                                                                                            \
+				Z_BREAKPOINT;                                                                                          \
 			}                                                                                                          \
 		} while (false)
 #else
@@ -101,6 +103,9 @@ using u8  = uint8_t;
 using u16 = uint16_t;
 using u32 = uint32_t;
 using u64 = uint64_t;
+
+using isize = intptr_t;
+using usize = uintptr_t;
 
 using f32 = float;
 using f64 = double;
@@ -149,20 +154,44 @@ inline const char* ReadFile(const char* filename)
 }
 
 #include <math.h>
+#include <numbers>
+
 // Math stuff
-constexpr f32 PI   = 3.14159265359f;
-constexpr f32 PI_2 = PI / 2.0f;
-constexpr f32 PI_3 = PI / 3.0f;
-constexpr f32 PI_4 = PI / 4.0f;
-constexpr f32 PI_8 = PI / 8.0f;
-constexpr f32 TAU  = 2.0f * PI;
+namespace F32
+{
+constexpr f32 Pi   = std::numbers::pi_v<f32>;
+constexpr f32 Pi_2 = Pi * 0.5f;
+constexpr f32 Pi_3 = Pi / 3.0f;
+constexpr f32 Pi_4 = Pi / 4.0f;
+constexpr f32 Pi_6 = Pi / 6.0f;
+constexpr f32 Pi_8 = Pi / 8.0f;
+constexpr f32 Tau  = Pi * 2.0f;
 
-constexpr f32 DEG_TO_RAD = PI / 180.0f;
-constexpr f32 RAD_TO_DEG = 180.0f / PI;
+constexpr f32 DegToRad = Pi / 180.0f;
+constexpr f32 RadToDeg = 180.0f / Pi;
 
-constexpr f32 SQRT_2 = 1.41421356237f;
-constexpr f32 E      = 2.71828182846f; // Euler's number
-constexpr f32 PHI    = 1.61803398875f; // Golden ratio
+constexpr f32 Sqrt_2 = std::numbers::sqrt2_v<f32>;
+constexpr f32 E      = std::numbers::e_v<f32>;
+constexpr f32 Phi    = std::numbers::phi_v<f32>;
+}
+
+namespace F64
+{
+constexpr f64 Pi   = std::numbers::pi_v<f64>;
+constexpr f64 Pi_2 = Pi * 0.5;
+constexpr f64 Pi_3 = Pi / 3.0;
+constexpr f64 Pi_4 = Pi / 4.0;
+constexpr f64 Pi_6 = Pi / 6.0;
+constexpr f64 Pi_8 = Pi / 8.0;
+constexpr f64 Tau  = Pi * 2.0;
+
+constexpr f64 DegToRad = Pi / 180.0;
+constexpr f64 RadToDeg = 180.0 / Pi;
+
+constexpr f64 Sqrt_2 = std::numbers::sqrt2_v<f64>;
+constexpr f64 E      = std::numbers::e_v<f64>;
+constexpr f64 Phi    = std::numbers::phi_v<f64>;
+}
 
 // clang-format off
 template <typename T> inline T Min(const T a, const T b) { return a < b ? a : b; }
@@ -172,16 +201,29 @@ template <typename T> inline T ClampTop(const T x, const T a) { return Min(x, a)
 template <typename T> inline T Clamp(const T x, const T a, const T b) { return ClampBot(a, ClampTop(x, b)); }
 template <typename T> inline T Saturate(const T x) { return Clamp(x, T{0}, T{1}); }
 
+template <typename T> inline T Abs(const T x) { return abs(x); }
+template <> inline f32 Abs(const f32 x) { return fabsf(x); }
+template <> inline f64 Abs(const f64 x) { return fabs(x); }
+
 inline f32 Sine(const f32 x) { return sinf(x); }
 inline f32 Cosine(const f32 x) { return cosf(x); }
 inline f32 Tangent(const f32 x) { return tanf(x); }
 inline f32 ArcSine(const f32 x) { return asinf(x); }
 inline f32 ArcCosine(const f32 x) { return acosf(x); }
 inline f32 ArcTangent(const f32 x) { return atanf(x); }
-
 inline f32 Pow(const f32 x, const f32 e) { return powf(x, e); }
 inline f32 Sqrt(const f32 x) { return sqrtf(x); }
+inline f32 ToRadians(const f32 x) { return x * F32::DegToRad; }
+inline f32 ToDegrees(const f32 x) { return x * F32::RadToDeg; }
 
-inline f32 ToRadians(f32 x) { return x * DEG_TO_RAD; }
-inline f32 ToDegrees(f32 x) { return x * RAD_TO_DEG; }
+inline f64 Sine(const f64 x) { return sin(x); }
+inline f64 Cosine(const f64 x) { return cos(x); }
+inline f64 Tangent(const f64 x) { return tan(x); }
+inline f64 ArcSine(const f64 x) { return asin(x); }
+inline f64 ArcCosine(const f64 x) { return acos(x); }
+inline f64 ArcTangent(const f64 x) { return atan(x); }
+inline f64 Pow(const f64 x, const f64 e) { return pow(x, e); }
+inline f64 Sqrt(const f64 x) { return sqrt(x); }
+inline f64 ToRadians(const f64 x) { return x * F64::DegToRad; }
+inline f64 ToDegrees(const f64 x) { return x * F64::RadToDeg; }
 // clang-format on
