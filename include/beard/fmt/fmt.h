35d6216c25f145549c47093e607dca0da548f280
Charly Mourglia <charly.mourglia@gmail.com>
2022-02-13 15:52:15 +0100

    Introduce a string tokenizer and a string to number converter

---
+++ include/beard/fmt/fmt.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <beard/misc/optional.h>
+
+#include <charconv>
+#include <optional>
+#include <string_view>
+#include <vector>
+
+namespace beard::fmt {
+std::vector<std::string_view> tokenize(std::string_view input,
+                                       std::string_view token,
+                                       bool skip_empty = true) {
+  std::vector<std::string_view> result;
+
+  auto curr_str = input;
+  usize offset;
+  while ((offset = curr_str.find_first_of(token)) != std::string_view::npos) {
+    if (offset != 0 || !skip_empty) {
+      result.push_back(curr_str.substr(0, offset));
+    }
+    curr_str = curr_str.substr(offset + 1);
+  }
+
+  return result;
+}
+
+template <typename T>
+inline beard::optional<T> parse_number(std::string_view input) {
+  T result;
+  auto start = input.data();
+  auto end = input.data() + input.size();
+  auto parse_result = std::from_chars(start, end, result);
+
+  if (parse_result.ec != std::errc{}) {
+    return {};
+  }
+
+  return result;
+}
+
+}  // namespace beard::fmt
--- tests/TestCompile.cpp
+++ tests/TestCompile.cpp
@@ -1,8 +1,11 @@
 #include <beard/containers/array.h>
 #include <beard/containers/hash_map.h>
 #include <beard/core/macros.h>
+#include <beard/fmt/fmt.h>
 #include <beard/misc/hash.h>
 #include <beard/misc/timer.h>
+
+#include <cassert>
 
 int main() {
   beard::hash_map<i32, real> a;
@@ -11,8 +14,17 @@ int main() {
 
   constexpr u32 hash = beard::crc32::hash("Hello !");
 
+  auto str = "12039813251203981";
+  auto v = beard::fmt::parse_number<i64>(str);
+  assert(v.has_value());
+  assert(v.value() == 12039813251203981);
+  assert(*v == 12039813251203981);
 
-	constexpr u32 hash = beard::Crc32::Hash("Hello !");
+  str = "1230981 421809 102983 328190831025 2130598  340912812 098 321098 ";
+  auto token_result = beard::fmt::tokenize(str, " ");
+  assert(token_result.size() == 8);
+  token_result = beard::fmt::tokenize(str, " ", false);
+  assert(token_result.size() == 9);
 
-	return 0;
+  return 0;
 }
